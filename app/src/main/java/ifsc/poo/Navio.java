b80a38423cf5e3be77a3bc0d2c2074abdfb981ee
Tiago Cargnin Morandi <ti061905@intelbras.com.br>
2025-04-29 18:16:34 -0300

    Feito pequenas mudanças

--- app/src/main/java/ifsc/poo/Navio.java
+++ app/src/main/java/ifsc/poo/Navio.java
@@ -1,17 +1,24 @@
 package ifsc.poo;
 import edu.princeton.cs.algs4.Draw;
 
-
 public class Navio {
+    
+    private final int CASA = 40;
+    private final int PIXEL = 50;
+    
     public void desenhar(Draw navio){
-        
 
-        for(int i = 130; i <= 290; i+=40){
+         for(int i = 130; i <= 290; i+=40){
         navio.filledSquare(170, i, 19.5);
         }
 
         for(int i = 90; i <=130; i+=40){
             navio.filledSquare(i, 370, 19.5);
         }
+ 
+    }
+    public void navio(String verthori, char simbolo, Double[][] xy ){
+        
+        
     }
 }
